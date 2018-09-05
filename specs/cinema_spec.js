c94be379195d31a4e8dfee5dcfa127e8f450f82d
nimbus117 <mail@nimbus117.co.uk>
2018-09-05 13:41:53 +0100

    passed 'should be able to check whether there are some films from a particular year' test

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -42,7 +42,11 @@ describe('Cinema', function () {
     const expected = [moonlight, trainspotting];
     assert.deepStrictEqual(actual, expected);
   });
-  it('should be able to check whether there are some films from a particular year');
+  it('should be able to check whether there are some films from a particular year', function () {
+    const actual = cinema.filterByYear(2016);
+    const expected = [moonlight];
+    assert.deepStrictEqual(actual, expected);
+  });
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
