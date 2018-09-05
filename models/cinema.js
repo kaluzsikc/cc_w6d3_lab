26bfd71396344e1953490b7b071187d9337fe965
nimbus117 <mail@nimbus117.co.uk>
2018-09-05 13:22:55 +0100

    passed 'should be able to get a list of film titles' test

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,10 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.titles = function() {
+  return this.films.map(film => film.title);
+}
+
+
+
 module.exports = Cinema;
